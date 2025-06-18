aaa2bde89544d81211538d5436af6a43a2f8b51d
AstroTaffer <tereshin.dd@gmail.com>
2025-06-18 15:57:01 +0500

    New valid command and general refactoring

--- bash_manuscripts/repoman.sh
+++ bash_manuscripts/repoman.sh
@@ -1,52 +1,108 @@
 #!/bin/bash
+# Repository hub manager
 
 usage()
 {
-	echo 'Execute Git TARGET command in every subdirectory (non-recursive):
-	check	-	git branch + git status
-	fetch	-	git fetch --prune --all
-	pull	-	[fetch] + git pull
-	stock	-	git checkout {default_branch}
-	reset	-	clean -dfx + git reset --hard HEAD'
+	echo 'Execute REPO_CMD command in every subdirectory that is a Git repository (non-recursive)
+
+    REPO_CMDs:
+	status	-	git branch + git status
+	fetch	-	git fetch -p --all
+	pull	-	[fetch] + git pull --ff-only
+	goto	-	git checkout
+	clean	-	git clean -dfx + git reset --hard HEAD
+
+    Options:
+    -p, --path      -   path to working directory (pwd otherwise)
+    -t, --target    -   checkout target for [goto] (default branch otherwise)'
 	exit 1
 }
 
-TARGET="$1"
-[ "$TARGET" == "" ] && usage
 
-WDIR="$2"
-[ "$WDIR" == "" ] && WDIR=$(pwd)
+# ----- READING PARAMETERS ----- #
+REPO_CMD=""
+WDIR=$(pwd)
+TARGET=""
+
+while (( $# > 0 )); do
+    case $1 in
+        -p)
+            ;&
+        --path)
+            shift
+            if (( $# == 0 )); then
+                echo -e "\e[31m""No path provided""\e[0m"
+                exit 1
+            else
+                WDIR=$1
+            fi
+            ;;
+        -t)
+            ;&
+        --target)
+            shift
+            if (( $# == 0 )); then
+                echo -e "\e[31m""No goto provided""\e[0m"
+                exit 1
+            else
+                TARGET=$1
+            fi
+            ;;
+        *)
+            if [[ ${REPO_CMD} == "" ]]; then
+                REPO_CMD=$1
+            else
+                echo -e "\e[31m""Too much REPO_CMDs""\e[0m"
+                exit 1
+            fi
+            ;;
+    esac
+
+    shift
+done
+
+[[ ${REPO_CMD} == "" ]] && usage
+
+
+# ----- EXECUTING REPO_CMD ----- #
 cd $WDIR
 
 for repo in */.git
 do
-	echo -e "\n\033[0;36m" "Entering ${repo%/*}:" "\033[0m"
+	echo -e "\n\e[0;36m" "Entering ${repo%/*}:" "\e[0m"
 	cd $repo/..
 
-	case $TARGET in
-		check)
+	case $REPO_CMD in
+		status)
 			git branch
 			git status
 			;;
 		fetch)
-			git fetch --prune --all --tags
+			git fetch -p --all --tags
 			;;
 		pull)
-			git fetch --prune --all --tags
+			git fetch -p --all --tags
 			git pull --ff-only
 			;;
-		stock)
-			default_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
-			git checkout $default_branch
+		goto)
+            if [[ ${TARGET} == "" ]]; then
+                checkout_target=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+            else
+                checkout_target=$TARGET
+            fi
+			git checkout $checkout_target
 			;;
-		reset)
+		clean)
 			git clean -dfx
 			git reset --hard HEAD
 			;;
+        *)
+            echo -e "\e[31m""Invalid command""\e[0m"
+            exit 1
 	esac
 
 	cd ..
 done
 
-echo -e "\n\033[1;32m" "Done!" "\033[0m"
+echo -e "\n\e[1;32m" "Done!" "\e[0m"
 
