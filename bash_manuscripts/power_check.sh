cc2a2b8c6a9ebeb9eac2c964c896bb661914f155
AstroTaffer <tereshin.dd@gmail.com>
2025-08-27 10:24:41 +0500

    Made function for toggle variables RES text

--- bash_manuscripts/power_check.sh
+++ bash_manuscripts/power_check.sh
@@ -12,14 +12,24 @@ CMAGENTA='\e[95m'
 BAT1_PATH='/sys/class/power_supply/BAT1'
 VPC_PATH='/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00'
 
-function color_by_value() {
-if (($1 > 66)); then
-    echo "${CGREEN}"
-elif (($1 > 33)); then
-    echo "${CYELLOW}"
-else
-    echo "${CRED}"
-fi
+function color_from_percentage() {
+    if (($1 > 66)); then
+        echo "${CGREEN}"
+    elif (($1 > 33)); then
+        echo "${CYELLOW}"
+    else
+        echo "${CRED}"
+    fi
+}
+
+function res_from_toggle() {
+    if [[ $1 == 1 ]]; then
+        echo "${CGREEN}ON${CRESET}"
+    elif [[ $1 == 0 ]]; then
+        echo "${CRED}OFF${CRESET}"
+    else
+        echo "${CMAGENTA}ERR${CRESET}"
+    fi
 }
 
 
@@ -45,33 +55,23 @@ STATUS_RES="${STATUS_RES}${STATUS}${CRESET}"
 CAPACITY=$(cat "${BAT1_PATH}/capacity")
 ENERGY_NOW=$(cat "${BAT1_PATH}/energy_now")
 ENERGY_FULL=$(cat "${BAT1_PATH}/energy_full")
-CAPACITY_RES="$(color_by_value $CAPACITY)${CAPACITY}%${CRESET} \
+CAPACITY_RES="$(color_from_percentage $CAPACITY)${CAPACITY}%${CRESET} \
 (${ENERGY_NOW}/${ENERGY_FULL} mcAh)"
 
 ENERGY_FULL_DESIGN=$(cat "${BAT1_PATH}/energy_full_design")
 CYCLE_COUNT=$(cat "${BAT1_PATH}/cycle_count")
 HEALTH=$((100 * ENERGY_FULL / ENERGY_FULL_DESIGN))
-HEALTH_RES="$(color_by_value $HEALTH)${HEALTH}%${CRESET} \
+HEALTH_RES="$(color_from_percentage $HEALTH)${HEALTH}%${CRESET} \
 (${ENERGY_FULL}/${ENERGY_FULL_DESIGN} mcAh, \
 ${CYCLE_COUNT} cycle)"
 
 
 # ----- GET ACPI REPORT ----- #
 CONS_MODE=$(cat "${VPC_PATH}/conservation_mode")
-if [[ ${CONS_MODE} == 1 ]]; then
-    CONS_MODE_RES="${CGREEN}ON"
-else
-    CONS_MODE_RES="${CRED}OFF"
-fi
-CONS_MODE_RES="${CONS_MODE_RES}${CRESET}"
+CONS_MODE_RES="$(res_from_toggle $CONS_MODE)"
 
 CAMERA_POWER=$(cat "${VPC_PATH}/camera_power")
-if [[ ${CAMERA_POWER} == 1 ]]; then
-    CAMERA_POWER_RES="${CGREEN}ON"
-else
-    CAMERA_POWER_RES="${CRED}OFF"
-fi
-CAMERA_POWER_RES="${CAMERA_POWER_RES}${CRESET}"
+CAMERA_POWER_RES="$(res_from_toggle $CAMERA_POWER)"
 
 
 # ----- DISPLAY RESULT ----- #
