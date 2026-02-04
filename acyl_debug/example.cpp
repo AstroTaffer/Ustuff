ea1abd412165b3644df7546c0bc4729b3c92a270
AstroTaffer <tereshin.dd@gmail.com>
2026-02-04 19:43:31 +0500

    Moved common C/C++ examples to the top of main() for aestetic purposes

--- acyl_debug/example.cpp
+++ acyl_debug/example.cpp
@@ -4,6 +4,14 @@ int main()
 {
     acyl::colored_marker("...The Net is vast and infinite...", acyl::Normal, acyl::Cyan, acyl::Black);
 
+    acyl::colored_marker("WARNING", acyl::Blinky, acyl::Yellow, acyl::Red);
+
+    int foo = 0x1337;
+    int bar = 0x7AFF;
+    acyl::set_style(acyl::Normal, acyl::Magenta, acyl::Blue);
+    std::cout << "foo = " << foo << ", bar = " << bar << std::endl;
+    acyl::reset_style();
+
     acyl::colored_marker("C++ API supports default arguments", acyl::Bright);
 
     acyl::show_output() = true;
@@ -12,14 +20,6 @@ int main()
         acyl::colored_ok("C++ API has global flag for conditional output");
     }
     acyl::show_output() = false;
-
-    acyl::colored_marker("WARNING", acyl::Blinky, acyl::Yellow, acyl::Red);
-
-    int foo = 0x1337;
-    int bar = 0x7AFF;
-    acyl::set_style(acyl::Normal, acyl::Magenta, acyl::Blue);
-    std::cout << "foo = " << foo << ", bar = " << bar << std::endl;
-    acyl::reset_style();
 
     return 0;
 }
